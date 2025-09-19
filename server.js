5b5f8040ed5d40233b6fce52ea628684964b6f65
Ujahcollins <ujahcollins@gmail.com>
2025-09-19 18:45:36 +0100

    chnaging the deposiit and investment llogic

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/kyc', kycRoutes);
 app.use("/api/investments", investmentRoutes);
 app.use("/", statsRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/api/admin", depositRoute);
+app.use("/api/deposit", depositRoute);
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
